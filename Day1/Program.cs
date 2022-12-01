24016ae1f09aa60103308f36cc79b4aa21abbcaa
Alkio, Antton <antton.alkio@if.fi>
2022-12-01 08:52:16 +0200

    day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,16 @@
+﻿static void Part1()
+{
+    var input = File.ReadAllText("input.txt").Split("\r\n\r\n").Select((x, i) => x.Split("\r\n").Select(int.Parse).Sum());
+
+    Console.WriteLine(input.Max());
+}
+
+static void Part2()
+{
+    var input = File.ReadAllText("input.txt").Split("\r\n\r\n").Select((x, i) => x.Split("\r\n").Select(int.Parse).Sum());
+
+    Console.WriteLine(input.OrderByDescending(x => x).Take(3).Sum());
+}
+
+Part1();
+Part2();
